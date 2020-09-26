7e538bcb3a26e0f535cbc933d6341b15e7b63325
csmer <csmer@users.noreply.github.com>
2020-09-26 12:37:08 -0700

    More CRUD function
    Routing for get by id, delete by id, update by id. Verified work through Insomnia.

--- backend/routes/listings.js
+++ backend/routes/listings.js
@@ -37,4 +37,34 @@ router.route('/add').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Listing.findById(req.params.id)
+      .then(listings => res.json(listings))
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Listing.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Listing deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Listing.findById(req.params.id)
+      .then(listings => {
+          listings.username = req.body.username;
+          listings.name = req.body.name;
+          listings.description = req.body.description;
+          listings.size = req.body.size;
+          listings.color = req.body.color;
+          listings.condition = req.body.condition;
+          listings.price = req.body.price;
+
+          listings.save()
+            .then(() => res.json('Listing updated.'))
+            .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
