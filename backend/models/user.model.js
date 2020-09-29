0553d684d2de191280644ab9f3bf4e8e27129b15
David <Imalreadysalty@users.noreply.github.com>
2020-09-28 21:57:23 -0700

    User.model - Added encryption, cleaned up
    Encryption added for passwords being stored in the database.  There was some commented out code that I deleted.  I added the library bcrypt.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,24 +1,21 @@
-/*var express = require("express");
-var bodyParser = require("body-parser");
-
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/gfg');
-var db = mongoose.connection;
-
-db.on('error', console.log.bind(console, "connection error"));
-db.once('open', function(callback){
-    console.log("connection succeeded");
-})
-*/
-
-const mongoose = require('mongoose');
-//require('mongoose-type-email');
-
-//mongoose.SchemaTypes.Email.defaults.message = 'Email address is invalid'
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+
+        username: {
+            type: String,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            required: [true, "Can not be blank"],
+            minlength : [3, "Name is too short!"],
+            maxlength: 13,
+            index: true
+
+        },
+
         firstname: {
             type: String,
             required: true,
@@ -33,73 +30,25 @@ const userSchema = new Schema({
 
         email: {
             type: String,
-            required: true
+            unique: true,
+            lowercase: true,
+            required: [true, "Can not be blank"],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'is invalid']
         },
 
         password: {
             type: String,
-            required: true
-
+            required: true,
+            minlength: [8, "Password too short!" ],
+           
         },
 
 }, {
         timestamps: true,
 })
 
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var app = express();
-
-app.use(bodyParser.json());
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.post('/sign_up', function(req, res) {             
-    var firstName = req.body.firstName;
-    var lastName  = req.body.lastName;
-    var email = req.body.email;
-    var pass = req.body.password;
-
-    var data = {
-        "firstName": firstName,
-        "lastName": lastName,
-        "email": email,
-        "pass": pass
-    }
-
-db.collection('details').insertOne(data, function(err, collection){
-    if (err) throw err;
-    console.log("Record inserted Successfully");
-
-});
-
-    return res.redirect('signup_success.html');
-})
-
-app.get('/', function(req, res) {                                       //We may have to change the html file
-    res.set({
-        'Access-control-Allow-Origin': '*'
-    });
-    return res.redirect('index.html');
-    
-}).listen(5000)
-
-console.log("server listening at port 5000");
-*/
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 let User = require('../models/user.model');
+var bcrypt = require('bcrypt');
+var saltLength = 10;
 
 router.route('/').get((req, res) => {
     User.find()
@@ -7,14 +9,22 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 
 });
+/* Need to create Delete and update for users
+and I need to make login as well
+*/
+router.route('/sign_up').post((req, res) => {          
 
-router.route('/add').post((req, res) => {
+    bcrypt.hash(req.body.password, saltLength, function (err, hash) {
+
+    const username = req.body.username;
     const firstname = req.body.firstname;
     const lastname = req.body.lastname;
     const email = req.body.email;
-    const password = req.body.password;
+    const password = hash;
 
     const newUser = new User({
+        
+        username,
         firstname,
         lastname,
         email,
@@ -25,6 +35,8 @@ router.route('/add').post((req, res) => {
     newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err))
+
+});
 
 });
 
