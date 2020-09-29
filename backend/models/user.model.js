7463eb857a3ac6164a2c5d982a7f6fd6be0761fd
David <Imalreadysalty@users.noreply.github.com>
2020-09-28 23:18:59 -0700

    User.js update & comments
    I created the routes for update and delete for a user.  Added comments to my code for description.

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+
+//Helps us connect to database 
+const mongoose = require('mongoose');  
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+//This is basically an object we created for user
+const userSchema = new Schema({        
 
         username: {
             type: String,
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,17 +1,27 @@
+
+//Allows us to create a route to the database from the server
+//Insomnia was used to test this
 const router = require('express').Router();
 let User = require('../models/user.model');
+
+//This will allow us to encrypt users passwords
 var bcrypt = require('bcrypt');
+//const { route } = require('./listings');
+
+//This will add a random salt at the end of our hashed password.
+//Allows for more security the higher it is , but computationally difficult
 var saltLength = 10;
 
+
+//This is how we will obtain info stored in the database
 router.route('/').get((req, res) => {
     User.find()
     .then(user => res.json(user))
     .catch(err => res.status(400).json('Error: ' + err));
 
 });
-/* Need to create Delete and update for users
-and I need to make login as well
-*/
+
+//Allows people to sign up and this is sent to the database
 router.route('/sign_up').post((req, res) => {          
 
     bcrypt.hash(req.body.password, saltLength, function (err, hash) {
@@ -38,6 +48,41 @@ router.route('/sign_up').post((req, res) => {
 
 });
 
+});
+
+
+//We can find a user by the unique id that is given to their account
+router.route('/:id').get((req, res) => {
+    User.findById(req.params.id)
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//In order to delete a users account we need to have their id
+router.route('/:id').delete((req, res) => {
+    User.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Listing deleted. '))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//This is used to update the users profile
+router.route('/update/:id').post((req, res) => {
+    User.findById(req.params.id)
+        .then(users => {
+            bcrypt.hash(req.body.password, saltLength, function (err, hash) {
+                users.username = req.body.username;
+                users.firstname = req.body.firstname;
+                users.lastname = req.body.lastname;
+                users.email = req.body.email;
+                users.password = hash;
+
+                users.save()
+                    .then(() => res.json('Listing updated.'))
+                    .catch(err => res.status(400).json('Error: ' + err));
+
+            });
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
