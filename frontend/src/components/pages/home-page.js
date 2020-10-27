935bec25ed267db799692194978038da6d632976
HarpreetGh <70241088+HarpreetGh@users.noreply.github.com>
2020-10-27 10:58:59 -0700

    HomePage works, Filter can break if sizes mixed between shoe, and garm

--- frontend/src/components/pages/home-page.js
+++ frontend/src/components/pages/home-page.js
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from "react";
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import { 
+  Button, Card, CardActions, CardContent,
+  CardMedia, CssBaseline, Grid, Typography,
+  Container, Fab, Link
+} from '@material-ui/core';
 import { //filter stuff
   FormLabel, FormControl, FormGroup,
   FormControlLabel, Checkbox
 } from '@material-ui/core';
 import Axios from "axios";
-import Link from "@material-ui/core/Link";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -147,14 +141,11 @@ export default function Album() {
         break;
     }
     setFilter(newFilter);
-    console.log(filter);
+    //console.log(filter);
     //loading();
     Axios.post("http://localhost:4000/listings/filter", filter)
-      .then(response => {
-        console.log(response.data);
-        setListings(response.data);
-        console.log(listings);
-      });
+      .then(response => { setListings([]);
+        setListings(response.data) });
     //setReload(!reload)
   }
 
